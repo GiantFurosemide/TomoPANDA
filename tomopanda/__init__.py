8ec6e9925ec29c1c9986d4e1dd6f98dbe4a5d108
MuWang <18640057101@163.com>
2025-10-20 16:27:01 +0200

    change version number

--- tomopanda/__init__.py
+++ tomopanda/__init__.py
@@ -3,9 +3,9 @@ TomoPANDA - CryoET膜蛋白检测工具
 基于SE(3)等变变换器的断层扫描数据分析工具
 """
 
-__version__ = "0.1.0"
-__author__ = "TomoPANDA Team"
-__email__ = "contact@tomopanda.org"
+__version__ = "0.1.2"
+__author__ = "Mu Wang"
+__email__ = "mu.wang@mdc-berlin.de"
 
 from .cli import main
 
